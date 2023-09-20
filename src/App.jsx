58d0ca8ecbde5cc8ca232ad6873f7b69c6970f65
Badirkhanov Rovshan Mukhlis <badirkhanov_r@itstep.edu.az>
2023-09-20 10:36:22 +0400

    Portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { createContext, useEffect, useReducer, useState } from 'react';
 import Border from './Components/Border/Border';
 import Paragraph from './Components/Paragraph/Paragraph';
 import Image from './Components/Image/Image';
@@ -7,42 +8,22 @@ import Button from './Components/Button/Button';
 import Form from './Components/Form/Form';
 import DivWithButton from './Components/DivWithButton/DivWithButton';
 import InputTypeText from './Components/Inputs/InputTypeText/InputTypeText';
-import { createContext, useEffect, useReducer, useState } from 'react';
+import NoPortalExample from './Components/NoPortalExample/NoPortalExample';
+import PortalExample from './Components/PortalExample/PortalExample';
+
 
 export const ThemeContext = createContext(null);
 export const ThemeToggleContext = createContext(null);
 
 function App() {
-  const inputsData = [
-    {
-      label: 'Name:',
-      defaultText: 'Name',
-      regexp: '\\w{3,10}',
-      name: 'name',
-    },
-    {
-      label: 'Surname:',
-      defaultText: 'Surname',
-      regexp: '\\w{3,10}',
-      name: 'surname',
-    },
-    {
-      label: 'Middle name:',
-      defaultText: 'Middle name',
-      regexp: '\\w{3,10}',
-      name: 'middle-name',
-    },
-    {
-      label: 'Middle name:',
-      // defaultText: 'Middle name',
-      regexp: '\\w{3,10}',
-      name: 'middle-name',
-    },
-  ];
-
   return (
     <>
-      <Form inputsData={inputsData}></Form>
+      <div className="clipping-container">
+        <NoPortalExample  />
+      </div>
+      <div className="clipping-container">
+        <PortalExample />
+      </div>
     </>
   );
 }
---
+++ src/Components/ModalContent/ModalContent.jsx
@@ -0,0 +1,12 @@
+function ModalContent({ onClose }) {
+    return (
+        <div className="modal-wrapper">
+            <div className="modal">
+                <div>I'm a modal dialog</div>
+                <button onClick={onClose}>Close</button>
+            </div>
+        </div>
+    );
+}
+
+export default ModalContent;
